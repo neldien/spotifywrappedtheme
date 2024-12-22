8a5d05dd4bdea4a9b990cd1d7e74774ac8d9f0a9
nour eldifrawy <12177432+neldien@users.noreply.github.com>
2024-12-22 13:13:05 -0800

    fixing something

--- server/worker.js
+++ server/worker.js
@@ -6,7 +6,12 @@ require('dotenv').config();
 const { videosDir } = require('./server');
 
 // Create a directory for storing videos if it doesn't exist
-
+if (process.env.WORKER_ROLE !== 'true') {
+    if (!fs.existsSync(videosDir)) {
+        fs.mkdirSync(videosDir);
+        console.log(`Server created videos directory at: ${videosDir}`);
+    }
+}
 console.log('Video generation worker started');
 
 videoQueue.process(async (job) => {
